06149d031fc0f890bd5eba680d5ca0ed24e1a653
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2024-11-18 23:43:26 +0500

    Update HomePage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,117 @@
+// import { NavLink } from 'react-router-dom';
+// import logo from '../Images/logo.png';
+// import './Navbar.css';
+// import { useEffect, useState } from 'react';
+// import { Drawer, Button } from 'antd';
+// import { MenuOutlined } from '@ant-design/icons';
+
+// export default function Navbar() {
+//   const [isScrolled, setIsScrolled] = useState(false);
+//   const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+
+//   // Function to handle scroll event
+//   const handleScroll = () => {
+//     if (window.scrollY > 25) {
+//       setIsScrolled(true);
+//     } else {
+//       setIsScrolled(false);
+//     }
+//   };
+
+//   // Attach scroll event listener
+//   useEffect(() => {
+//     window.addEventListener('scroll', handleScroll);
+
+//     // Cleanup the event listener on component unmount
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   // Drawer handlers
+//   const showDrawer = () => {
+//     setIsDrawerVisible(true);
+//   };
+
+//   const closeDrawer = () => {
+//     setIsDrawerVisible(false);
+//   };
+
+//   return (
+//     <div className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+//       <div className="leftnav">
+//         <div className="logo">
+//           <NavLink to="/">
+//             <img src={logo} alt="logo" />
+//           </NavLink>
+//         </div>
+
+//         {/* Drawer for Mobile View */}
+        
+//         {/* Desktop View Links */}
+//         <div className="navlinks">
+//           <NavLink className="nav-link" to={'/method'}>
+//             Method
+//           </NavLink>
+//           <NavLink className="nav-link" to={'/featured-tutors'}>
+//             Tutors
+//           </NavLink>
+//           <NavLink className="nav-link" to={'/courses'}>
+//             Courses
+//           </NavLink>
+//           <NavLink className="nav-link" to={'/resources'}>
+//             Resources
+//           </NavLink>
+//           <NavLink className="nav-link" to={'/kids'}>
+//             Kids
+//           </NavLink>
+//           <NavLink className="nav-link" to={'/business'}>
+//             Business
+//           </NavLink>
+//         </div>
+//       </div>
+//       <div className='rightdiv'>
+      
+
+//       <div className="navbuttons">
+//         <button>Login</button>
+//         <button>Signup</button>
+//       </div>
+//       <div className="mobile-menu">
+//           <Button type="text" icon={<MenuOutlined />} onClick={showDrawer} className='drawerbutton' />
+//           <Drawer
+//             title="Menu"
+//             placement="right"
+//             onClose={closeDrawer}
+//             visible={isDrawerVisible}
+//           >
+//           <div className="mobile-navlinks" >
+//             <NavLink className="nav-link" to={'/method'} onClick={closeDrawer}>
+//               Method
+//             </NavLink>
+//             <NavLink className="nav-link" to={'/featured-tutors'} onClick={closeDrawer}>
+//               Tutors
+//             </NavLink>
+//             <NavLink className="nav-link" to={'/courses'} onClick={closeDrawer}>
+//               Courses
+//             </NavLink>
+//             <NavLink className="nav-link" to={'/resources'} onClick={closeDrawer}>
+//               Resources
+//             </NavLink>
+//             <NavLink className="nav-link" to={'/kids'} onClick={closeDrawer}>
+//               Kids
+//             </NavLink>
+//             <NavLink className="nav-link" to={'/business'} onClick={closeDrawer}>
+//               Business
+//             </NavLink>
+//             </div>
+//           </Drawer>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import { NavLink } from 'react-router-dom';
 import logo from '../Images/logo.png';
 import './Navbar.css';
@@ -45,10 +159,6 @@ export default function Navbar() {
             <img src={logo} alt="logo" />
           </NavLink>
         </div>
-
-        {/* Drawer for Mobile View */}
-        
-        {/* Desktop View Links */}
         <div className="navlinks">
           <NavLink className="nav-link" to={'/method'}>
             Method
@@ -70,39 +180,44 @@ export default function Navbar() {
           </NavLink>
         </div>
       </div>
-      <div className='rightdiv'>
-      
-
-      <div className="navbuttons">
-        <button>Login</button>
-        <button>Signup</button>
-      </div>
-      <div className="mobile-menu">
-          <Button type="text" icon={<MenuOutlined />} onClick={showDrawer} className='drawerbutton' />
+      <div className="rightdiv">
+        <div className="navbuttons">
+          <button>Login</button>
+          <button>Signup</button>
+        </div>
+        <div className="mobile-menu">
+          <Button
+            type="text"
+            icon={<MenuOutlined />}
+            onClick={showDrawer}
+            className="drawerbutton"
+          />
           <Drawer
             title="Menu"
             placement="right"
             onClose={closeDrawer}
             visible={isDrawerVisible}
           >
-            <NavLink className="nav-link" to={'/method'} onClick={closeDrawer}>
-              Method
-            </NavLink>
-            <NavLink className="nav-link" to={'/featured-tutors'} onClick={closeDrawer}>
-              Tutors
-            </NavLink>
-            <NavLink className="nav-link" to={'/courses'} onClick={closeDrawer}>
-              Courses
-            </NavLink>
-            <NavLink className="nav-link" to={'/resources'} onClick={closeDrawer}>
-              Resources
-            </NavLink>
-            <NavLink className="nav-link" to={'/kids'} onClick={closeDrawer}>
-              Kids
-            </NavLink>
-            <NavLink className="nav-link" to={'/business'} onClick={closeDrawer}>
-              Business
-            </NavLink>
+            <div className="mobile-navlinks">
+              <NavLink className="nav-link" to={'/method'} onClick={closeDrawer}>
+                Method
+              </NavLink>
+              <NavLink className="nav-link" to={'/featured-tutors'} onClick={closeDrawer}>
+                Tutors
+              </NavLink>
+              <NavLink className="nav-link" to={'/courses'} onClick={closeDrawer}>
+                Courses
+              </NavLink>
+              <NavLink className="nav-link" to={'/resources'} onClick={closeDrawer}>
+                Resources
+              </NavLink>
+              <NavLink className="nav-link" to={'/kids'} onClick={closeDrawer}>
+                Kids
+              </NavLink>
+              <NavLink className="nav-link" to={'/business'} onClick={closeDrawer}>
+                Business
+              </NavLink>
+            </div>
           </Drawer>
         </div>
       </div>
