c33e4e26e6b80ec97891c776597c515c892a530a
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2024-11-20 05:01:29 +0500

    Update Home Page

--- src/Pages/HomePage/Components/Price.jsx
+++ src/Pages/HomePage/Components/Price.jsx
@@ -1,121 +1,3 @@
-// import "./Price.css";
-// export default function Price() {
-//   const pricepackage = [
-//     {
-//       name: "Small groups",
-//       description: "Real-world practice in groups",
-//       actualprice: "$10/mo",
-//       discountedprice: "$9",
-//       offer: [
-//         "Work with a tutor and 1-2 students",
-//         "Meet learners from around the world",
-//         "Structured discussions for all levels",
-//         "For adults 21+",
-//       ],
-//     },
-//     {
-//       name: "Private+",
-//       description: "Full access to private and group lessons",
-//       actualprice: "$52/mo",
-//       discountedprice: "$26",
-//       offer: [
-//         "Our most comprehensive experience",
-//         "Take 100% private lessons or try groups too",
-//         "Rewatch your lesson videos",
-//         "Get personalized exercises between lessons",
-//       ],
-//     },
-//     {
-//       name: "Kids",
-//       description: "For kids under 18",
-//       actualprice: "$119/mo",
-//       discountedprice: "$61",
-//       offer: [
-//         "1:1 time with specialized tutors",
-//         "Active learning through conversation",
-//         "Beginner to advanced courses included",
-//         "Fun games and activities",
-//       ],
-//     },
-//   ];
-//   return (
-//     <div className="pricesection">
-//       <h1>Start today. Learn for life.</h1>
-//       <div className="pricecards">
-//         <div className="card">
-//           <div className="head">
-//             <h2>Small Group</h2>
-//             <p>Hey thtere</p>
-//           </div>
-//           <button>Select</button>
-//           <div className="pricetag">
-//             <p>STARTING FROM</p>
-//             <span>$10/month</span>
-//             <h2>
-//               $9{" "}
-//               <sup>
-//                 {" "}
-//                 <small>/mon </small>
-//               </sup>{" "}
-//             </h2>
-//           </div>
-//           <div className="offers">
-//             <li>Work with a tutor and 1-2 students</li>
-//             <li>Meet learners from around the world</li>
-//             <li>Structured discussions for all levels</li>
-//           </div>
-//         </div>
-//         <div className="card">
-//           <div className="head">
-//             <h2>Small Group</h2>
-//             <p>Hey thtere</p>
-//           </div>
-//           <button>Select</button>
-//           <div className="pricetag">
-//             <p>STARTING FROM</p>
-//             <span>$10/month</span>
-//             <h2>
-//               $9{" "}
-//               <sup>
-//                 {" "}
-//                 <small>/mon </small>
-//               </sup>{" "}
-//             </h2>
-//           </div>
-//           <div className="offers">
-//             <li>Work with a tutor and 1-2 students</li>
-//             <li>Meet learners from around the world</li>
-//             <li>Structured discussions for all levels</li>
-//           </div>
-//         </div>
-//         <div className="card">
-//           <div className="head">
-//             <h2>Small Group</h2>
-//             <p>Hey thtere</p>
-//           </div>
-//           <button>Select</button>
-//           <div className="pricetag">
-//             <p>STARTING FROM</p>
-//             <span>$10/month</span>
-//             <h2>
-//               $9{" "}
-//               <sup>
-//                 {" "}
-//                 <small>/mon </small>
-//               </sup>{" "}
-//             </h2>
-//           </div>
-//           <div className="offers">
-//             <li>Work with a tutor and 1-2 students</li>
-//             <li>Meet learners from around the world</li>
-//             <li>Structured discussions for all levels</li>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import "./Price.css";
 
 export default function Price() {
@@ -180,12 +62,18 @@ export default function Price() {
               </h2>
             </div>
             <div className="offers">
+            <ul>
               {pkg.offer.map((item, i) => (
                 <li key={i}>{item}</li>
               ))}
+              </ul>
             </div>
           </div>
         ))}
+      </div>
+      <div className="bottomcontent" >
+      <p>Not ready to commit yet? Explore all The English Hub has to offer without a subscription</p>
+      <button>Explore The English Hub</button>
       </div>
     </div>
   );
