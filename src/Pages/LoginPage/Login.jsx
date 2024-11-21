085633fa3249a30e3c83f288806493bdda342fe5
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2024-11-22 02:30:59 +0500

    Update Login Page

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,26 +1,28 @@
 import "./Login.css";
-
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../Backend/loginwithgoogle";
+import googlelogo from "../../Images/google.png";
+import logo from '../../Images/logo.png'
+import { Link } from "react-router-dom";
 const Login = () => {
+  function loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider).then(async (result) => {
+      console.log(result);
+    });
+  }
+
   return (
     <div className="login-container">
       <div className="login-card">
+        <div className="logo" ><img src={logo} alt="" /></div>
         <h1 className="welcome-text">Welcome back!</h1>
 
         <div className="social-login">
-  <button className="social-button google">
-    <i className="fab fa-google"></i> 
-  </button>
-  <button className="social-button apple">
-    <i className="fab fa-apple"></i> 
-  </button>
-  <button className="social-button facebook">
-    <i className="fab fa-facebook"></i> 
-  </button>
-  <button className="social-button wechat">
-    <i className="fab fa-weixin"></i> 
-  </button>
-</div>
-
+          <button onClick={loginWithGoogle} className="social-button google">
+            <img src={googlelogo} alt="Google logo" /> Continue with Google
+          </button>
+        </div>
 
         <div className="separator">
           <span>OR</span>
@@ -48,7 +50,7 @@ const Login = () => {
         </form>
 
         <div className="signup">
-          Dont have an account? <a href="#">Sign up</a>
+          Dont have an account? <Link to="/signup">Sign up</Link>
         </div>
       </div>
     </div>
