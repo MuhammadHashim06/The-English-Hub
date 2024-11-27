942efface81011d3edf2ac6a42e77dcb80b557c7
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2024-11-28 03:01:03 +0500

    Updated Account

---
+++ src/Pages/MethodPage/components/data/methodData.js
@@ -0,0 +1,35 @@
+// HeroSectionData.js
+
+import conversation from "../../../../Images/Conversation.jpg";
+import feedback from "../../../../Images/feedback.png";
+import individual from "../../../../Images/image.png";
+
+export const bottomSectionData = [
+  {
+    title: "Conversation Based Learning",
+    description:
+      "If you want to become fluent in English, there’s no way around speaking with real people. From day one on Cambly, you jump into real conversations about topics that matter to you. Whatever your level, whatever your goal.",
+    image: conversation,
+    imageAlt: "Conversation Image",
+    extraText:
+      "Choose from thousands of tutors with different backgrounds, industries, accents, and more—so you can talk about what really matters to you.",
+  },
+  {
+    title: "Feedback tailored to you",
+    description:
+      "Every conversation is filled with your own unique learning opportunities. We help you identify and learn from your mistakes, and test your skills as you go.",
+    image: feedback,
+    imageAlt: "Feedback Image",
+    extraText:
+      "Choose from thousands of tutors with different backgrounds, industries, accents, and more—so you can talk about what really matters to you.",
+  },
+  {
+    title: "Individualized",
+    description:
+      "Rather than learning through one-size-fits-all vocabulary and generic grammar rules, we create activities based on your lessons—made just for you. You get to practice on your own time, so you’re ready for your next conversation.",
+    image: individual,
+    imageAlt: "Individualized Learning Image",
+    extraText:
+      "Choose from thousands of tutors with different backgrounds, industries, accents, and more—so you can talk about what really matters to you.",
+  },
+];
